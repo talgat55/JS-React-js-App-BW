0adfafec9183f948033679fc2ed8c85038b55bbb
talgat55 <lightxdesign55@gmail.com>
2019-08-26 22:08:23 +0600

    add logic login

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,17 +1,29 @@
 import React, {Fragment} from 'react';
 import LoginForm from '../forms/LoginForm';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {login}  from '../../actions/auth';
 
 class LoginPage extends React.Component {
-    render()
-    {
+    submit = data => this.props.login(data).then(() => this.props.history.push("/"));
+
+    render() {
+
         return (
             <Fragment>
                 <h1>Login page </h1>
-                <LoginForm  />
+                <LoginForm submit={this.submit}/>
             </Fragment>
         );
-    }
-    ;
-}
+    };
+};
 
-export default LoginPage;
+LoginPage.PropTypes = {
+    history: PropTypes.shape({
+        push: PropTypes.func.isRequired
+    }).isRequired,
+    login: PropTypes.func.isRequired
+};
+
+
+export default  connect( null, {login})(LoginPage);
--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 import 'semantic-ui-css/semantic.min.css'
 import * as serviceWorker from './serviceWorker';
+import {applyMiddleware, createStore} from 'redux';
+import thunk from 'redux-thunk';
+import {Provider}  from 'react-redux';
+import rootReducer from './rootReducer';
+import {composeWithDevTools} from 'redux-devtools-extension';
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(<BrowserRouter>
-    <App />
+    <Provider store={store}>
+        <App/>
+    </Provider>
 </BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
